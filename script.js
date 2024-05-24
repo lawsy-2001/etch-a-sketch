9d9c2ed65c74aff8b178fec41a96dff9fa0eaaaa
Andrew <166739925+lawsy-2001@users.noreply.github.com>
2024-05-24 10:48:45 +0100

    Re-size button completed
    programed resize button to allow user to enter a
    number between 1 and 100 and this will clear the
    excising grid and replace it with one of the
    chosen size

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+// draw grid with default of 16 x 16
 const gridContainer = document.querySelector('.grid-container');
 
-function drawGrid(squaresPerRow) {
+function drawGrid(squaresPerRow = 16) {
   for (i = 0; i < squaresPerRow; i++) {
     const row = document.createElement('div');
     row.className = 'row';
@@ -13,14 +14,31 @@ function drawGrid(squaresPerRow) {
       row.appendChild(col);
     }
   }
+  // creates the drawing effect
+  const gridBox = document.querySelectorAll('#grid-box');
+
+  gridBox.forEach((box) => {
+    box.addEventListener('mouseenter', () =>
+      box.setAttribute('style', 'background: black;')
+    );
+  });
 }
 
-drawGrid(25);
+drawGrid();
 
-const gridBox = document.querySelectorAll('#grid-box');
+// re-size button code
+const btnResize = document.querySelector('#resize');
 
-gridBox.forEach((box) => {
-  box.addEventListener('mouseenter', () =>
-    box.setAttribute('style', 'background: black;')
-  );
+btnResize.addEventListener('click', () => {
+  const userGridSize = parseInt(prompt('Choose a size between 1 and 100'));
+  if (
+    userGridSize > 100 ||
+    userGridSize < 1 ||
+    typeof userGridSize !== 'number'
+  ) {
+    alert('Not a valid entry. Please select a size between 1 and 100');
+  } else {
+    gridContainer.replaceChildren();
+    drawGrid(userGridSize);
+  }
 });
